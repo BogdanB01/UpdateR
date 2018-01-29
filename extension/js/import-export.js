8ddb2206c11de3edf1c5b0351b35907bbe7c07d2
mariusolaru <mariusolaru1997@yahoo.com>
2018-01-29 13:05:58 +0200

    Implemented functionality in import/export tab

--- extension/js/import-export.js
+++ extension/js/import-export.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-	
+
+	var cs = new ChromeStore();
+    cs.init(1024 * 1024 * 1024);
+
+	function getDirnameFromUrl(url) {
+        return url.replace(/\/|:|\?|"|\<|\>|\.|\*|\|/g, '_');
+    }
 
 	function processFile(file){
 		var reader = new FileReader();
@@ -11,34 +17,50 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	    		try {
 	    			let settings = JSON.parse(contents);
-	    			console.log(settings);
+	    			/*console.log(settings);*/
 					
 					Object.keys(settings).forEach(function (key) {
 
 						if(key != 'color' && key != 'links'){
-
 							document.getElementById('drop-error').innerHTML = '<p> Invalid JSON format !</p>';
-
 						}
 
 					});	
 
 					var color = settings.color;
-
-					console.log(color);
+					var links = settings.links;
 
 					chrome.storage.sync.set({"color": color}, function(){
-			            console.log('saved color');
-
 			            var event = new Event('change');
 			            document.querySelector("#color-picker").dispatchEvent(event);
 
 			        });
 
-	    			
+					var regex = RegExp('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})');
+
+		            chrome.storage.sync.get({list: []}, function(data){
+
+		            	for(var i = 0 ; i < links.length; i++){
+
+							var entry = links[i];
+
+							if(entry.match(regex)){
+								
+								addLinkToList(entry , data);
+
+		                    }else{
+
+		                    	document.getElementById('drop-error').innerHTML += '<p>Invalid URL format! -> ' + links[i] + '</p>';
+
+		                    }
+
+	                	}
+                    });
+
 	    		} catch (error) {
+	    			console.log("Error= " + error);
 	    			console.log('invalid json!');
-	    			document.getElementById('drop-error').innerHTML = '<p> Invalid JSON !</p>';
+	    			document.getElementById('drop-error').innerHTML = '<p> Invalid JSON!</p>';
 	    		}
 	    	}
 	    	reader.readAsText(file);
@@ -83,17 +105,77 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	document.getElementById('export-settings').addEventListener('click', function() {
 		var element = document.createElement('a');
-		var obj = { color : "red" , 
-					links : ["link1" , "link2"]}
 
-		element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(obj)));
+		chrome.storage.sync.get('color' , function(data1){
 
-		element.setAttribute('download', 'settings.json');
+			var color = data1.color;
 
-		element.style.display = 'none';
-		document.body.appendChild(element);
+			chrome.storage.sync.get({list: []}, function(data2){
+				var urls = [];
+				for(var i = 0 ; i < data2.list.length; i++){
+					urls.push(data2.list[i].url);
+				}
 
-		element.click();
-		document.body.removeChild(element);
+				var obj = {
+					color: color,
+					links: urls
+				};
+				console.log(obj);	
+				element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(obj)));
+
+				element.setAttribute('download', 'settings.json');
+
+				element.style.display = 'none';
+				document.body.appendChild(element);
+
+				element.click();
+				document.body.removeChild(element);
+
+			});
+
+		})
+
+		
 	});
+
+	function addLinkToList(entry, data){
+
+		var currentdate = new Date();
+		var datetime = currentdate.getDate() + "/"
+            + (currentdate.getMonth()+1)  + "/" 
+            + currentdate.getFullYear() + " "  
+            + currentdate.getHours() + ":"  
+            + currentdate.getMinutes() + ":" 
+            + currentdate.getSeconds();
+
+        var row = {
+            url: entry,
+            time: datetime
+        };
+
+
+        if(!chrome.runtime.error){
+            data.list.push(row);
+
+            chrome.storage.sync.set({
+                list:data.list
+            }, function(){
+                 console.log('added to list');
+             });
+        }
+
+        var dirname = getDirnameFromUrl(row.url);
+    	//create directory to store screenshots
+
+        cs.getDir('screenshots/'+dirname, {create : true}, function() {
+            console.log('Created dir', dirname);
+        });
+
+        cs.ls('screenshots', function(arr) {
+            for(var i = 0; i < arr.length; i++)
+                console.log(arr[i].name);
+        });
+
+	}
+
 });
